c58d6600ef6b9dfff0f1a94ec6ec204f389c2f77
ElHuaco <alexhuac@gmail.com>
2021-07-06 22:22:57 +0200

    Added serv_conf struct. Loop over client list now.

--- mini_serv.c
+++ mini_serv.c
@@ -12,6 +12,15 @@ typedef struct		s_list
 	struct s_list	*next = NULL;
 }					t_list;
 
+typedef struct		s_serv_conf
+{
+	fd_set			read_fds;
+	fd_set			read_master;
+	fd_set			write_fds;
+	fd_set			write_master;
+	int				max;
+}					t_serv_conf;
+
 static void	fatal_exit(void)
 {
 	char *str = "Fatal error\n";
@@ -19,33 +28,41 @@ static void	fatal_exit(void)
 	exit(1);
 }
 
-static int	create_server_listener(unsigned short int port)
+t_serv_conf	load_server_conf(unsigned short int port)
 {
 	//TODO: Crear socket listener en 127.0.0.1
 	//bzero, sockaddr
 	//socket, bind, listen
+	t_serv_conf serv;
+	serv.max = listener;
+	FD_ZERO(&serv.read_fds);
+	FD_ZERO(&serv.read_master);
+	FD_SET(listener, &serv.read_master);
+	FD_ZERO(&serv.write_fds);
+	FD_ZERO(&serv.write_master);
+	FD_SET(listener, &serv.write_master);
+	return (serv);
 }
 
-static void	add_client(fd_set *reads, fd_set *writes, int *max,
-		t_list *clients)
+static void	add_client(t_serv_conf *serv, t_list **clients)
 {
 	//TODO: accept(), actualizar max, añadir a lista y FD_SET
 }
 
-static void	remove_client(fd_set *reads, fd_set *writes, int *max,
-		t_list *clients)
+static void	remove_client(t_serv_conf *serv, t_list **clients)
 {
 	//TODO: close(), actualizar max, quitar de la lista y FD_CLR
 }
 
-static void	send_messages(char *buff, fd_set *writes, t_list *clients)
+static void	send_messages(int sender, char *buff, fd_set *write_fds,
+	t_list **clients)
 {
-	//TODO: sprintf(%d) a cada línea de buff
-	t_list *temp = client->next;
+	//TODO: sprintf(client %d: ) a cada línea de buff
+	t_list *temp = (*client)->next;
 	while (temp != NULL)
 	{
-		if (FD_ISSET(temp->fd, &write_fds) &&
-			send(temp->fd, buff, sizeof buff, 0) == -1)
+		if (FD_ISSET(temp->fd, &write_fds) && temp->fd != sender
+			&& send(temp->fd, buff, sizeof buff, 0) == -1)
 			fatal_exit();
 		temp = temp->next;
 	}
@@ -59,51 +76,40 @@ int	main(int argc, char **argv)
 		write(2, str, strlen(str));
 		exit(1);
 	}
-	unsigned short int port = atoi(argv[1]);
-	int listener = create_server_listener(port);
-	int max = listener;
-
-	fd_set read_fds;
-	fd_set read_master;
-	FD_ZERO(&read_fds);
-	FD_ZERO(&read_master);
-	FD_SET(listener, &read_master);
-
-	fd_set write_fds;
-	fd_set write_master;
-	FD_ZERO(&write_fds);
-	FD_ZERO(&write_master);
-	FD_SET(listener, &write_master);
-
-	t_list clients;
-	clients.fd = listener;
+	t_serv_conf serv = load_server_conf(atoi(argv[1]));
+	t_list *clients;
+	if (!(clients = malloc(sizeof t_list)))
+		fatal_exit();
+	clients->fd = listener;
+	t_list *temp;
 	while(1)
 	{
-		FD_COPY(&read_master, &read_fds);
-		FD_COPY(&write_master, &write_fds);
-		if (select(max + 1, &read_fds, &write_fds, NULL, NULL) == -1)
+		FD_COPY(&serv.read_master, &serv.read_fds);
+		FD_COPY(&serv.write_master, &serv.write_fds);
+		if (select(serv.max + 1, &serv.read_fds, &serv.write_fds, 0, 0) == -1)
 			fatal_exit();
-		//cambiar for por iterar sobre lista? Ya que se empieza en listener
-		for (int i = 0; i <= max; ++i)
+		temp = clients;
+		while (temp != NULL)
 		{
-			if (!FD_ISSET(i, &read_fds))
+			if (!FD_ISSET(temp->fd, &serv.read_fds))
 				continue ;
-			if (i == listener)
-				add_client(&read_master, &write_master, &max, &clients);
+			if (temp->fd == listener)
+				add_client(&serv, &clients);
 			else
 			{
 				char buff[4096];
 				memset(buff, 0, sizeof buff);
 				int nbytes;
-				if ((nbytes = recv(i, buff, sizeof buff, 0) <= 0))
+				if ((nbytes = recv(temp->fd, buff, sizeof buff, 0) <= 0))
 				{
 					if (nbytes != 0)
 						fatal_exit();
-					remove_client(&read_master, &write_master, &max, &clients);
+					remove_client(&serv, &clients);
 				}
 				else
-					send_messages(buff, &write_fds, &clients);
+					send_messages(temp->fd, buff, &serv->write_fds, &clients);
 			}
+			temp = temp->next;
 		}
 	}
 }
